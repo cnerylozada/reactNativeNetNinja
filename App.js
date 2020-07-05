d140be1f2d85d6c5e1007a5a7a744c34b474c291
unknown <cnerylozada@gmail.com>
2020-07-05 11:40:22 -0500

    feat: initial

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { key: "1", name: "cristh" },
+    { key: "2", name: "tatiana" },
+    { key: "3", name: "lucciano" },
+    { key: "4", name: "julieta" },
+    { key: "5", name: "diana" },
+    { key: "6", name: "lucia" },
+    { key: "7", name: "josefina" },
+    { key: "8", name: "gabriela" },
+    { key: "9", name: "stephany" },
+    { key: "10", name: "ana" },
+    { key: "111", name: "esther" },
+  ]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
