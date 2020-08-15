bc364ddce3dacc5974ed594c8c9c2e5acee5fbf3
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-08-15 12:21:40 -0500

    feat: dismiss keyboard and reset implemented in game form

--- components/modal-form.jsx
+++ components/modal-form.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Modal, View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import {
+  Modal,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { globalStyles } from "../styles/global";
 import CustomButton from "../_commons/custom-button";
@@ -13,52 +21,55 @@ const ModalForm = ({ isModalVisible, setVisibility, addNewGameTitle }) => {
     rating: "",
   };
 
-  const saveGameForm = (value) => {
-    setVisibility(false);
+  const saveGameForm = (value, actions) => {
     addNewGameTitle(value);
+    actions.resetForm();
+    setVisibility(false);
   };
   return (
     <Modal visible={isModalVisible} animationType="slide">
-      <View style={[globalStyles.content]}>
-        <Text style={{ marginVertical: 30, textAlign: "center" }}>
-          <FontAwesome
-            name="window-close"
-            size={30}
-            style={styles.closeModalIcon}
-            onPress={() => setVisibility(false)}
-          />
-        </Text>
-        <View>
-          <Formik initialValues={gameForm} onSubmit={saveGameForm}>
-            {({ handleChange, values, handleSubmit }) => (
-              <>
-                <CustomInput
-                  name="title"
-                  value={values.title}
-                  handleChange={handleChange}
-                  placeholder="Review title"
-                />
-                <CustomInput
-                  name="detail"
-                  value={values.detail}
-                  handleChange={handleChange}
-                  placeholder="Review details"
-                />
-                <CustomInput
-                  name="rating"
-                  value={values.rating}
-                  handleChange={handleChange}
-                  placeholder="Rating (1 -5)"
-                  keyboardType="numeric"
-                />
-                <TouchableOpacity onPress={handleSubmit}>
-                  <CustomButton title="submit" />
-                </TouchableOpacity>
-              </>
-            )}
-          </Formik>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={[globalStyles.content]}>
+          <Text style={{ marginVertical: 30, textAlign: "center" }}>
+            <FontAwesome
+              name="window-close"
+              size={30}
+              style={styles.closeModalIcon}
+              onPress={() => setVisibility(false)}
+            />
+          </Text>
+          <View>
+            <Formik initialValues={gameForm} onSubmit={saveGameForm}>
+              {({ handleChange, values, handleSubmit }) => (
+                <>
+                  <CustomInput
+                    name="title"
+                    value={values.title}
+                    handleChange={handleChange}
+                    placeholder="Review title"
+                  />
+                  <CustomInput
+                    name="detail"
+                    value={values.detail}
+                    handleChange={handleChange}
+                    placeholder="Review details"
+                  />
+                  <CustomInput
+                    name="rating"
+                    value={values.rating}
+                    handleChange={handleChange}
+                    placeholder="Rating (1 -5)"
+                    keyboardType="numeric"
+                  />
+                  <TouchableOpacity onPress={handleSubmit}>
+                    <CustomButton title="submit" />
+                  </TouchableOpacity>
+                </>
+              )}
+            </Formik>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
